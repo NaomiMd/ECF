50b6663d1f868b72f7940b38f297948deef95156
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-03-31 13:43:21 +0200

    create navbar

---
+++ templates/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg bg-body">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        <a class="nav-link" href="#">Carte & Menu</a>
+        <a class="nav-link" href="#">Réserver</a>
+        <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></a>
+
+      </div>
+    </div>
+  </div>
+</nav>
