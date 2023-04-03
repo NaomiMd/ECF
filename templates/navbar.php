44623ac163689f75aa51e00f4ffa9cd5da1c0e8e
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-03 18:40:54 +0200

    add fixed-top class + change logo link by the generateLink function

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,16 +1,31 @@
-<nav class="navbar navbar-expand-lg bg-body">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
-        <a class="nav-link" href="#">Carte & Menu</a>
-        <a class="nav-link" href="#">Réserver</a>
-        <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></a>
-
-      </div>
+<header>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+        <div class="container">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="mx-auto"></div>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="index.html">Accueil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Galerie.html">Carte & Menu</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Tarifs.html">Réserver</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div class="container-fluid" id="banner-img">
+        <div>
+          <img class="logoheader" src="<?= generateLink('assets/logo.png') ?>" alt="logo">
+        </div>
     </div>
-  </div>
-</nav>
+      </header>
