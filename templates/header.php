14eb3b00e732a27ff229b4d324ecbf8fd7748bbf
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-03 18:39:26 +0200

    change the link of style.css by the function generateLink

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Restaurant le Quai Antique">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="<?= generateLink('assets/style.css'); ?>">
     <title>Quai Antique</title>
 </head>
 <body>
