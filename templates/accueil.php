f0442b8d05b5d0069092a3545966f6abd6912b5e
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-03 18:38:28 +0200

    add pic, presentation text and style

---
+++ templates/accueil.php
@@ -0,0 +1,22 @@
+<div class="container-fluid text-center" id="presentation" >
+    <p>
+Inspiré par les saveurs savoyardes, le chef Arnaud Michant vous partage <br/>
+ses nouvelles créations à travers des plats gastronomiques. <br/>
+Le Quai Antique vous propose au déjeuner <br/>
+comme au dîner une expérience gastronomique, à travers une cuisine sans artifice.
+    </p>
+</div>
+
+<div class="container-fluid cuisine">
+    <div class="text">
+    <h4 class="title">Une cuisine réconfortante </h4>
+    <hr>
+    <p>
+    Nos chefs explorent les classiques de la gastronomie <br/> savoyarde pour les remettre au goût du jour.<br/>
+    Ainsi, la cuisine que nous proposons<br/> offre le réconfort des produits de savoie.</p>
+</div>
+<div class="restaurant">
+    <img src="<?= generateLink('assets/restaurant.jpg') ?>" alt="img" class="img-cuisine" >
+  </div>
+</div>
+</div> 
