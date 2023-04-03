c89fc4cd314cfd1ad0a27a9ebeead95f7993035c
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-03 19:05:15 +0200

    add equipe part

--- templates/accueil.php
+++ templates/accueil.php
@@ -19,4 +19,25 @@ comme au dîner une expérience gastronomique, à travers une cuisine sans artif
     <img src="<?= generateLink('assets/restaurant.jpg') ?>" alt="img" class="img-cuisine" >
   </div>
 </div>
-</div> 
+
+
+<div class="container-fluid cuisine">
+<div class="restaurant">
+    <img src="<?= generateLink('assets/chef1.jpg') ?>" alt="img" class="img-chef" >
+  </div>
+  <div class="text">
+    <h4 class="title">L'équipe</h4>
+    <hr>
+    <p>
+    L'équipe au sein de notre restaurant<br/>
+    tant en cuisine qu'en salle
+    s'efforce de donner le <br/>
+    meilleur d'elle-même chaque jour
+    pour vous permettre de vivre <br/>
+    la meilleure expérience gastronomique
+    possible.
+    </p>
+
+</div>
+</div>
+ 
