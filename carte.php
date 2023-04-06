8150ed97de7a5b8d9d1984b9d36b635270f6ed86
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-06 20:37:57 +0200

    add carte page

---
+++ carte.php
@@ -0,0 +1,13 @@
+<?php
+require_once("config.php");
+require_once _ROOT_ . '\templates\header.php';
+require_once _ROOT_ . '\templates\navbar.php';
+require_once _ROOT_ . '\templates\carte-menu.php';
+
+?>
+
+
+<?php
+require_once _ROOT_ . '\templates\footer.php';
+
+?>
---
+++ templates/carte-menu.php
@@ -0,0 +1,75 @@
+<div class="container-fluid" id="banner-carte">
+        <div>
+          <h1 class="pagetitle" >Carte & Menus</h1>
+        </div>
+</div>
+
+<div class="container-fluid menus">
+<h3 class="text-center mt-4" id="titlemenu">Les Menus</h3>
+<div class="underline"></div>
+<div class="block">
+<div class="container text-center mt-5">
+    <h5 class="menuTitle">Menus déjeuner</h5>
+    <div class="line"></div>
+    <div class="menu mt-4">
+        <p>Déjeuner 3 services</p>
+        <p class="italic" >(Entrée, plat, dessert)</p>
+        <p>40€</p>
+    </div>
+</div>
+
+<div class="container text-center mt-5">
+    <h5 class="menuTitle">Menus dîner</h5>
+    <div class="line"></div>
+    <div class="menu mt-4">
+        <p>Dîner 6 services</p>
+        <p class="italic" >(Amuse bouche, 2 entrée, plat, fromage, dessert)</p>
+        <p>120€</p>
+    </div>
+</div>
+</div>
+</div>
+<div class="line mt-5"></div>
+<div class="container carte mb-5">
+    <h3 class="text-center mt-5" id="titlecarte">La Carte</h3>
+    <div class="underline"></div>
+
+    <div class="accordion mt-5 mb-5">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        Entrée
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Titre - Description - Prix
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Plats
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Titre - Description - Prix      
+    </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Dessert
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Titre - Description - Prix      
+    </div>
+    </div>
+  </div>
+</div>
+</div>
