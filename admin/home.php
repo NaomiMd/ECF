17900194358d921046b623503cf71177121457c3
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-18 12:26:05 +0200

    add footer

--- admin/home.php
+++ admin/home.php
@@ -1,8 +1,13 @@
 <?php
 require_once("../config.php");
 require_once _ROOT_ . '\templates\header.php';
-require_once _ROOT_ . '\admin\navadmin.php'
+require_once _ROOT_ . '\admin\navadmin.php';
 ?>
 <!-- isset(connexion) -->
 <h1 class="text-center homeadmin" >Bienvenue dans l'administration du Quai Antique</h1>
 <p class="text-center" >Vous pouvez ici gérer l'administration du quai antique</p>
+
+
+<?php 
+require_once _ROOT_ . '\admin\footer.php';
+?>
