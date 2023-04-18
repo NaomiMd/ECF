e16358297930206f052a1a69d3b7dc0b758edfe7
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-18 12:24:59 +0200

    add logout btn

--- admin/navadmin.php
+++ admin/navadmin.php
@@ -1,23 +1,22 @@
 <header>
 
-<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Quai Antique</a>
+    <a class="navbar-brand" href="<?= generateLink('admin/home.php') ?>">Quai Antique</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link" href="#">Galerie</a>
-        <a class="nav-link" href="#">Horaires</a>
-        <a class="nav-link" href="#">Menu et Carte</a>
-        <a class="nav-link" href="#">Réservation</a>
+      <div class="navbar-nav" >
+        <a class="nav-link" href="<?= generateLink('admin/galerie.php') ?>">Galerie</a>
+        <a class="nav-link" href="<?= generateLink('admin/horaires.php') ?>">Horaires</a>
+        <a class="nav-link" href="<?= generateLink('admin/carte_menu.php') ?>">Menu et Carte</a>
+        <a class="nav-link" href="<?= generateLink('admin/reservation.php') ?>">Réservation</a>
+<!-- renvoie à logout.php -->
+    <a class="nav-link" aria-current="page" href="logout.php">Se deconnecter</a>
       </div>
-    </div>
-    <div class="text-end">
-      <button type="button" class="btn btn-warning">Se déconnecter</button>
     </div>
   </div>
 </nav>
   
-  </header>
+</header>
