44765d48c5753caa484083a074c9c5956dfced3d
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-18 11:49:25 +0200

    add navbar for admin page

---
+++ admin/navadmin.php
@@ -0,0 +1,23 @@
+<header>
+
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Quai Antique</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="#">Galerie</a>
+        <a class="nav-link" href="#">Horaires</a>
+        <a class="nav-link" href="#">Menu et Carte</a>
+        <a class="nav-link" href="#">Réservation</a>
+      </div>
+    </div>
+    <div class="text-end">
+      <button type="button" class="btn btn-warning">Se déconnecter</button>
+    </div>
+  </div>
+</nav>
+  
+  </header>
