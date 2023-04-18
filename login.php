9d9e53dd3b5048016c69f7a3c5d3a88196662397
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-18 15:53:45 +0200

    add link to register

--- login.php
+++ login.php
@@ -12,16 +12,21 @@ require_once _ROOT_ . '\templates\navbar.php';
 </div>
 
 <div class="container" id="connexionform" >
+<h3 class="text-center mt-4">Se connecter</h3>
 <div class="mb-3 mt-4">
   <label for="email" class="form-label">Adresse mail</label>
   <input type="email" name="email" class="form-control" id="email" placeholder="nom@example.com">
 </div>
 <div class="mb-3">
-<label for="password" class="form-label">Password</label>
-<input type="password" name="password" id="password" class="form-control">
+<label for="confirmedpassword" class="form-label">Mot de passe</label>
+<input type="password" name="confirmedpassword" id="confirmedpassword" class="form-control">
 </div>
 <div class="mb-3">
-<button type="submit" class="btn mb-3 mt-2" id="btnconnexion" >Se connecter</button>
+<button type="submit" class="btn btnconnexion">Se connecter</button>
+</div>
+<hr>
+<div class="mb-3">
+<p>Pas encore inscrit ? <a href="<?= generateLink('register.php') ?>" class="btn btnCreate ">Créer un compte</a> </p>
 </div>
 </div>
 
@@ -30,5 +35,4 @@ require_once _ROOT_ . '\templates\navbar.php';
 
 <?php
 require_once _ROOT_ . '\templates\footer.php';
-
 ?>
