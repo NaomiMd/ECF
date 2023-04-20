cdaab3bdecb343afd4c227bb6601185490b8d868
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-20 18:45:34 +0200

    add a page menu

--- admin/navadmin.php
+++ admin/navadmin.php
@@ -11,7 +11,8 @@
       <a class="nav-link" href="<?= generateLink('admin/home.php') ?>">Accueil</a>
         <a class="nav-link" href="<?= generateLink('admin/Galerie/galerie.php') ?>">Galerie</a>
         <a class="nav-link" href="<?= generateLink('admin/Horaire/horaires.php') ?>">Horaires</a>
-        <a class="nav-link" href="<?= generateLink('admin/Carte_menu/carte_menu.php') ?>">Menu et Carte</a>
+        <a class="nav-link" href="<?= generateLink('admin/Carte/carte.php') ?>">Carte</a>
+        <a class="nav-link" href="<?= generateLink('admin/Menu/menu.php') ?>">Menu</a>
         <a class="nav-link" href="<?= generateLink('admin/Reservation/reservation.php') ?>">Réservation</a>
 <!-- renvoie à logout.php -->
     <a class="nav-link" aria-current="page" href="logout.php">Se deconnecter</a>
