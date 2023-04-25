1bcc5af5bbafcfb9632c92f1eaec21356d89d0fd
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-25 12:14:17 +0200

    add the php into carte-menu page + style

--- templates/carte-menu.php
+++ templates/carte-menu.php
@@ -1,3 +1,21 @@
+<?php
+require_once _ROOT_ . '\Controller\CategoryController.php';
+require_once _ROOT_ . '\Controller\FormuleController.php';
+require_once _ROOT_ . '\Controller\DishController.php';
+require_once _ROOT_ . '\Controller\MenuController.php';
+
+$CategoryController = new CategoryController();
+$categories = $CategoryController->getAll();
+
+$DishController = new DishController();
+$dishes = $DishController->getAll();
+
+$FormuleController = new FormuleController();
+$formules = $FormuleController->getAll();
+
+$MenuController = new MenuController();
+
+?>
 <div class="container-fluid" id="banner-carte">
         <div>
           <h1 class="pagetitle" >Carte & Menus</h1>
@@ -7,69 +25,49 @@
 <div class="container-fluid menus">
 <h3 class="text-center mt-4" id="titlemenu">Les Menus</h3>
 <div class="underline"></div>
+<?php
+foreach($formules as $formule) :
+  $menu = $MenuController->getIdMenu($formule->getMenu_Id());
+?>
 <div class="block">
 <div class="container text-center mt-5">
-    <h5 class="menuTitle">Menus déjeuner</h5>
+    <h5 class="menuTitle">Menu <?= $menu->getTitle(); ?></h5>
     <div class="line"></div>
     <div class="menu mt-4">
-        <p>Déjeuner 3 services</p>
-        <p class="italic" >(Entrée, plat, dessert)</p>
-        <p>40€</p>
+        <p><?= $formule->getDescription(); ?></p>
+        <p class="italic" ><?= $formule->getPrice(); ?>€</p>
     </div>
+</div>
+</div>
+<?php endforeach ; ?>
 </div>
 
-<div class="container text-center mt-5">
-    <h5 class="menuTitle">Menus dîner</h5>
-    <div class="line"></div>
-    <div class="menu mt-4">
-        <p>Dîner 6 services</p>
-        <p class="italic" >(Amuse bouche, 2 entrée, plat, fromage, dessert)</p>
-        <p>120€</p>
-    </div>
-</div>
-</div>
-</div>
 <div class="line mt-5"></div>
-<div class="container carte mb-5">
+<div class="container mb-5">
     <h3 class="text-center mt-5" id="titlecarte">La Carte</h3>
     <div class="underline"></div>
+        
+    <?php 
+    foreach($categories as $category) :
+    $category->getTitle();
+    ?>
 
-    <div class="accordion mt-5 mb-5">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-        Entrée
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        Titre - Description - Prix
-      </div>
+<div class="block">
+<div class="container text-center">
+    <h5 class="menuTitle"><?= $category->getTitle(); ?></h5>
+    <div class="line"></div>
+    <?php foreach($dishes as $dish) : ?>
+    <div class="carteBox">
+        <?php
+         if($dish->getCategory_Id() == $category->getId()) : ?>
+        <h5 class="mt-3" ><?= $dish->getTitle(); ?></h5>
+        <p><?= $dish->getDescription() ?> - <?= $dish->getPrice(); ?> €</p>
+        <hr class="separateur">
+        <?php endif ?>
     </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Plats
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-      Titre - Description - Prix      
-    </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Dessert
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-      Titre - Description - Prix      
-    </div>
-    </div>
-  </div>
+    <?php endforeach; ?>
 </div>
+
+</div>
+<?php endforeach; ?>
 </div>
