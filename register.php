23923c017d7b5a46c0639e3debcacca03ce44010
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-26 12:31:33 +0200

    add register page

--- register.php
+++ register.php
@@ -2,8 +2,11 @@
 require_once("config.php");
 require_once _ROOT_ . '\templates\header.php';
 require_once _ROOT_ . '\templates\navbar.php';
+require_once _ROOT_ . '\Controller\UserController.php';
 
+$userController = new UserController();
 ?>
+
 <div class="container-fluid" id="banner-login">
         <div>
           <h1 class="pagetitle">S'inscrire</h1>
@@ -11,8 +14,29 @@ require_once _ROOT_ . '\templates\navbar.php';
 </div>
 </div>
 
+<?php 
+
+if($_POST)
+{
+  if($_POST['password'] === $_POST['confirmPassword'])
+  {
+    unset($_POST['confirmPassword']);
+    $_POST['pasword'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $newUser = new User($_POST);
+    $userController->createUser($newUser);
+    $_SESSION['email'] = $_POST['email'];
+    echo"<script>window.location.href='index.php'</script>";
+  }else{
+    echo"<h5 class='text-center'>Le mot de passe ne correspond pas à celui entré</h5>";
+  }
+}
+
+?>
+
 <div class="container" id="connexionform">
     <h3 class="text-center mt-4">S'inscrire</h3>
+
+<form method="post">
 <div class="mb-3 mt-4">
   <label for="email" class="form-label">Adresse mail</label>
   <input type="email" name="email" class="form-control" id="email" placeholder="nom@example.com" required >
@@ -22,25 +46,31 @@ require_once _ROOT_ . '\templates\navbar.php';
 <input type="password" name="password" id="password" class="form-control" required >
 </div>
 <div class="mb-3">
-<label for="password" class="form-label">Confirmer votre mot de passe</label>
-<input type="password" name="password" id="password" class="form-control" required>
+<label for="confirmPassword" class="form-label">Confirmer votre mot de passe</label>
+<input type="password" name="confirmPassword" id="password" class="form-control" required>
 </div>
 <div class="mb-3  ">
-<label for="convives">Nombre de convives</label>
-<select class="form-select" name="convives" aria-label="Default select example" required>
+<label for="number_of_guest">Nombre de convives</label>
+<select class="form-select" name="number_of_guest" aria-label="Default select example" required>
   <option selected>Selectionner</option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
+  <option value="1">1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+  <option value="5">5</option>
+  <option value="6">6</option>
 </select>
 </div>
 <div class="mb-3 mt-4">
-    <label for="alergy">Mention des allergies</label>
-    <textarea class="form-control" name="alergy" id="alergy" cols="10" rows="5"></textarea>
+    <label for="allergy">Mention des allergies</label>
+    <textarea class="form-control" placeholder="exemples: poissons, arachides, gluten..." name="allergy" id="allergy" cols="10" rows="5"></textarea>
 </div>
+
+
 <div class="mb-3">
 <button type="submit" class="btn btnconnexion">S'inscrire</button>
 </div>
+</form>
 </div>
 
 
