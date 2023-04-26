68c1090ae5959b498116ad1e090624d316e935fb
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-04-26 12:31:16 +0200

    add user modal and controller

---
+++ Controller/UserController.php
@@ -0,0 +1,96 @@
+<?php
+require_once _ROOT_ . '/Modal/User.php';
+
+class UserController
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        try{
+            $this->pdo = new PDO('mysql:dbname=quai_antique;host=localhost;charset=utf8mb4', 'root', 'root');
+        }catch(Exception $e){
+            echo('Erreur : ' .$e->getMessage());
+        }
+    }
+
+public function getPdo()
+{
+    return $this->pdo;
+}
+public function setPdo(PDO $pdo)
+{
+    $this->pdo = $pdo;
+    return $this;
+}
+
+public function getAll(): array
+{
+    $users = [];
+    $req = $this->pdo->query("SELECT * FROM `user`");
+    $data = $req->fetchAll();
+    foreach($data as $user)
+    {
+        $users[] = new User($user);
+    }
+    return $users;
+}
+
+public function getUserId($id): User
+{
+    $req = $this->pdo->prepare("SELECT * FROM `user` WHERE id=:id");
+    $req->bindParam(":id", $id, PDO::PARAM_INT);
+    $req->execute();
+    $data = $req->fetch();
+    $user = new User($data);
+    return $user;
+}
+
+public function createUser(User $newUser)
+{
+    $req = $this->pdo->prepare("INSERT INTO `user` (email, password, allergy, number_of_guest, role) VALUES (:email, :password, :allergy, :number_of_guest, :role)");
+    $hashedPassword = password_hash($newUser->getPassword(), PASSWORD_DEFAULT);
+    $role = 'subscriber';
+    $req->bindValue(":email", $newUser->getEmail(), PDO::PARAM_STR);
+    $req->bindValue(":password", $hashedPassword, PDO::PARAM_STR);
+    $req->bindValue(":allergy", $newUser->getAllergy(), PDO::PARAM_STR);
+    $req->bindValue(":number_of_guest", $newUser->getNumber_of_guest(), PDO::PARAM_INT);
+    $req->bindValue(":role", $role, PDO::PARAM_STR);
+    $req->execute();
+}
+
+
+public function addUser(string $email, string $password, string $allergy,  $number_of_guest){
+    $sql = "INSERT INTO `user` (`email`, `password`, `allergy`, `number_of_guest`, `role`) VALUES (:email, :password, :allergy, :number_of_guest, :role);";
+    $query = $this->pdo->prepare($sql);
+    $password = password_hash($password, PASSWORD_DEFAULT); //permet de hasher le ps sur la bdd et empecher le hack //
+    $role = 'subscriber';
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->bindParam(':password', $password, PDO::PARAM_STR);
+    $query->bindParam(':number_of_guest', $number_of_guest, PDO::PARAM_INT);
+    $query->bindParam(':allergy', $allergy, PDO::PARAM_STR);
+    $query->bindParam(':role', $role, PDO::PARAM_STR);
+    $query->execute();
+}
+
+public function deleteUser(int $id)
+{
+    $req = $this->pdo->prepare("DELETE FROM `user` WHERE id=:id");
+    $req->bindValue(":id", $id, PDO::PARAM_INT);
+    $req->execute();
+}
+
+public function verifyUserLogin(string $email, string $password)
+{
+    $req= $this->pdo->prepare("SELECT * FROM `user` WHERE email=:email");
+    $req->bindParam(':email', $email, PDO::PARAM_STR);
+    $req->execute();
+    $user = $req->fetch();
+    if($user && password_verify($password, $user['password']))
+    {
+        return $user;
+    }else{
+        return false;
+    }
+}
+}
---
+++ Modal/User.php
@@ -0,0 +1,153 @@
+<?php
+
+class User
+{
+    private $id;
+    private $email;
+    private $password;
+    private $allergy;
+    private $number_of_guest;
+    private $role = 'subscriber';
+
+    public function __construct(array $data)
+    {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data)
+    {
+        foreach($data as $key => $value)
+        {
+            $method = 'set'.ucfirst($key);
+            
+            if(method_exists($this, $method))
+            {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        if(strlen($password) < 8)
+        {
+            echo 'Votre mot de passe doit être composé de 8 characters';
+        }
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of allergy
+     */ 
+    public function getAllergy()
+    {
+        return $this->allergy;
+    }
+
+    /**
+     * Set the value of allergy
+     *
+     * @return  self
+     */ 
+    public function setAllergy($allergy)
+    {
+        $this->allergy = $allergy;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of number_of_guest
+     */ 
+    public function getNumber_of_guest()
+    {
+        return $this->number_of_guest;
+    }
+
+    /**
+     * Set the value of number_of_guest
+     *
+     * @return  self
+     */ 
+    public function setNumber_of_guest($number_of_guest)
+    {
+        $this->number_of_guest = $number_of_guest;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of role
+     */ 
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set the value of role
+     *
+     * @return  self
+     */ 
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+}
