8016f3c743b57455fdb37f74e94c74a2b2e4bdf7
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-05-10 12:18:33 +0200

    change the img of the logo(

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
 <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 border-top" > 
     <div class="col mb-3">
-      <p class="m-5"> <img class="logo-footer" src="<?= generateLink('assets/img/logo.png') ?>" alt="logo"></p>
+      <p class="m-5"> <img class="logo-footer" src="<?= generateLink('assets/img/logo.svg') ?>" alt="logo"></p>
     </div>
 
     <div class="col mb-3"></div>
@@ -37,5 +37,9 @@
   </footer>
 </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+  <script src="<?= generateLink('assets/Js/jquery.js') ?>"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js" integrity="sha512-ux1VHIyaPxawuad8d1wr1i9l4mTwukRq5B3s8G3nEmdENnKF5wKfOV6MEUH0k/rNT4mFr/yL+ozoDiwhUQekTg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.full.min.js" integrity="sha512-AIOTidJAcHBH2G/oZv9viEGXRqDNmfdPVPYOYKGy3fti0xIplnlgMHUGfuNRzC6FkzIo0iIxgFnr9RikFxK+sw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>  <script src="<?= generateLink('assets/Js/jquery-ui.min.js') ?>"></script>
+  <script src="<?= generateLink('assets/Js/main.js') ?>"></script>
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,6 +11,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.min.css" integrity="sha512-f0tzWhCwVFS3WeYaofoLWkTP62ObhewQ1EZn65oSYDZUg1+CyywGKkWzm8BxaJj5HGKI72PnMH9jYyIFz+GH7g==" crossorigin="anonymous" referrerpolicy="no-referrer" />    <link rel="stylesheet" href="<?= generateLink('assets/jquery-ui.structure.min.css'); ?>">
+    <link rel="stylesheet" href="<?= generateLink('assets/jquery-ui.theme.min.css'); ?>">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
     <link rel="stylesheet" href="<?= generateLink('assets/style.css'); ?>">
     <title>Quai Antique</title>
 </head>
