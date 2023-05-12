dd2bc53b1f1a48e442ce9b5d0044cdaaac358cea
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-05-12 16:44:59 +0200

    change pics

--- templates/accueil.php
+++ templates/accueil.php
@@ -13,26 +13,26 @@ $galeries = $galerieController->getAll();
 
 
 <div class="container-fluid text-center" id="presentation" >
-    <h2>restaurant gastronomique à chambéry</h2>
-    <h4>Distingué de 2 étoiles au Guide Michelin</h4>
+    <h4>restaurant gastronomique à chambéry</h4>
+    <h5>Distingué de 2 étoiles au Guide Michelin</h5>
     <div class="highlight" ></div>
 </div>
 <!-- présentation du chef -->
 
 <div class="container-fluid cuisine">
-<div>
     <img src="<?= generateLink('assets/img/chef.jpg') ?>" alt="Le chef Arthur Michant" class="img-chef" >
-  </div>
   <div class="text">
-    <h2>Le Chef</h2>
-    <h4>Arthur Michant</h4>
+    <h4>Le Chef</h4>
+    <h2>Arthur Michant</h2>
     <div class="highlight"></div>
     <p>
     Le chef Arnaud Michant vous invite à découvrir une cuisine 
     remplie de saveur inspiré de la Savoie.<br>
     Sa passion pour la cuisine a commencé dès son plus jeune âge, 
     lorsqu'il aidait sa grand-mère à préparer des plats traditionnels 
-    à base de produits frais et locaux. <br/>
+    à base de produits frais et locaux. </p>
+    <p></p>
+    <p>
     Sa philosophie de cuisine est de mettre en valeur les ingrédients 
     locaux et de saison, en utilisant des techniques modernes pour créer des plats innovants et savoureux. <br/>
     Son talent et sa créativité ont été récompensés par de nombreux 
@@ -41,9 +41,9 @@ $galeries = $galerieController->getAll();
 </div>
 </div>
 
-<div class="container text-center">
+<div class="container text-center mb-5">
     <div class="verticalRight">
-    <h3 class="carteTitle" >Découvrez la carte</h3>
+    <h4 class="carteTitle" >Découvrez la carte</h4>
     </div>
 <div class="verticalLeft">
 <p>Notre Chef vous convie à partager sa cuisine et ses nouvelles créations pour le déjeuner et dîner.</p>
@@ -54,28 +54,21 @@ $galeries = $galerieController->getAll();
 
 
 
-<div class="container-fluid cuisine" id="team">
+<div class="container-fluid mt-5 cuisine">
     <div class="text">
-    <h4 >L'équipe</h4>
+    <h2>L'équipe</h2>
     <div class="highlight"></div>
     <p>
-    Notre équipe est composée de passionnés de la gastronomie, qui travaillent ensemble pour offrir une expérience culinaire de classe mondiale à nos clients. <br/>
-    Chaque membre de notre équipe apporte une expertise unique, créant une atmosphère dynamique et collaborative dans notre restaurant. <br/>
-    Notre personnel de service est également une partie essentielle de notre équipe, offrant un service attentionné et professionnel à nos clients. <br/>
-    Ils sont formés pour répondre aux besoins et aux demandes spécifiques de nos clients, offrant une expérience personnalisée et mémorable.
-</p>
+    Notre équipe est composée de passionnés de la gastronomie, qui travaillent ensemble pour vous offrir une expérience culinaire de classe mondiale. <br/>
+    Chaque membre de notre équipe apporte une expertise unique, créant une atmosphère dynamique et collaborative dans notre restaurant. </p>
+    <p>Notre personnel de service est également une partie essentielle de notre équipe, vous offrant un service attentionné et professionnel. <br/>
+    Ils sont formés pour répondre à vos besoins et vos demandes spécifiques, pour vous offrir une expérience personnalisée et mémorable.
+    </p>
 </div>
-<div class="restaurant">
     <img src="<?= generateLink('assets/img/equipe.jpg') ?>" alt="img" class="img-cuisine" >
-  </div>
 </div>
 
 <!-- GALERIE -->
-
-<div class="container text-center" id="galerie" >
-    <h3>Galerie</h3>
-</div>
-
 <div id="grid">
     
     <?php
