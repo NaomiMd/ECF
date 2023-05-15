3772a8aac9950f8c44d278459944c25b9a9dad06
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-05-15 17:09:25 +0200

    add confirmation page for booking

---
+++ templates/reservationConfirmation.php
@@ -0,0 +1,8 @@
+<?php
+require_once('../config.php');
+require_once _ROOT_ . '\templates\header.php';
+?>
+<script src="<?= generateLink('assets/Js/script.js');?>"></script>
+
+<h5 class="text-center mt-5" >Votre réservation a été prise en compte. Vous allez etre redirigé vers l'accueil.</h5>
+
