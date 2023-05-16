94879c920f0ba3846d9179c05380a308d2f05a1a
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-05-16 11:53:11 +0200

    delete comments

--- index.php
+++ index.php
@@ -3,10 +3,7 @@ require_once("config.php");
 require_once _ROOT_ . '\templates\header.php';
 require_once _ROOT_ . '\templates\navbar.php';
 require_once _ROOT_ . '\templates\accueil.php';
-
 ?>
-
-
 <?php
 require_once _ROOT_ . '\templates\footer.php';
 ?>
